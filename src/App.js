5daf8b684d56a8ca49e3d3ea259682e3cce34435
Patel Aneri <142383756+Aneri2009311@users.noreply.github.com>
2025-04-30 13:59:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import List from './List';
+
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
-let name="Courses:";
-let name2="Development:";
+
   
   return (
     <>
